5a9ab1ca918214c1c32a46adf386a1830b5c3748
Luis Daniel Bianchi <luisdaniel.bianchicordova@gmail.com>
2024-03-29 22:31:01 +0800

    test finding the PR for the branch

--- index.js
+++ index.js
@@ -14,26 +14,39 @@ try {
     core.setFailed(error.message);
 }
 
-
 async function run() {
     try {
+        const github_token = core.getInput('GITHUB_TOKEN');
+        const octokit = github.getOctokit(github_token);
+        const context = github.context;
+
+        // Get branch name from ref
+        const branchName = context.ref.replace('refs/heads/', '');
+
+        // Find pull requests associated with the branch
+        const { data: pullRequests } = await octokit.rest.pulls.list({
+            ...context.repo,
+            state: 'open',
+            head: `${context.repo.owner}:${branchName}`
+        });
+
+        if (pullRequests.length === 0) {
+            core.setFailed('No related pull request found.');
+            return;
+        }
+
+        // Assuming you want to comment on the first related pull request
+        const pullRequestNumber = pullRequests[0].number;
+
+        // Your message
         const message = "|---|---|---|\n" +
             "| [lib/src/cbor/cbor\\_reader/cbor\\_reader\\_collections.c](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup#diff-bGliL3NyYy9jYm9yL2Nib3JfcmVhZGVyL2Nib3JfcmVhZGVyX2NvbGxlY3Rpb25zLmM=) | 87.50% | [1 Missing :warning: ](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup) |\n" +
             "| [lib/src/cbor/cbor\\_reader/cbor\\_reader\\_numeric.c](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup#diff-bGliL3NyYy9jYm9yL2Nib3JfcmVhZGVyL2Nib3JfcmVhZGVyX251bWVyaWMuYw==) | 0.00% | [1 Missing :warning: ](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup) |"
 
-        const github_token = core.getInput('GITHUB_TOKEN');
 
-        const context = github.context;
-        if (context.payload.pull_request == null) {
-            core.setFailed('No pull request found.');
-            return;
-        }
-        const pull_request_number = context.payload.pull_request.number;
-
-        const octokit = new github.GitHub(github_token);
-        const new_comment = octokit.issues.createComment({
+        await octokit.rest.issues.createComment({
             ...context.repo,
-            issue_number: pull_request_number,
+            issue_number: pullRequestNumber,
             body: message
         });
 
