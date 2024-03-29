94c08c311b3778faa3aadbfbd47ab24b0baadcf4
Luis Daniel Bianchi <luisdaniel.bianchicordova@gmail.com>
2024-03-29 23:21:32 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -30878,29 +30878,46 @@ try {
     core.setFailed(error.message);
 }
 
+async function updateMISRAComment(octokit, context, newCommentBody) {
+    const { owner, repo } = context.repo;
+    const issue_number = context.issue.number; // Pull request number is the same as issue number
+
+    // Fetch all comments on the pull request
+    const { data: comments } = await octokit.rest.issues.listComments({
+        owner,
+        repo,
+        issue_number,
+    });
+
+    // Find the comment with the hidden tag
+    const existingComment = comments.find(comment => comment.body.includes('<!-- MISRA C REPORT -->'));
+
+    if (existingComment) {
+        // Update the existing comment
+        await octokit.rest.issues.updateComment({
+            owner,
+            repo,
+            comment_id: existingComment.id,
+            body: newCommentBody,
+        });
+        console.log('Updated the existing MISRA C report comment.');
+    } else {
+        // Create a new comment if no existing comment was found
+        await octokit.rest.issues.createComment({
+            owner,
+            repo,
+            issue_number,
+            body: newCommentBody,
+        });
+        console.log('Created a new MISRA C report comment.');
+    }
+}
+
 async function run() {
     try {
         const github_token = core.getInput('GITHUB_TOKEN');
         const octokit = github.getOctokit(github_token);
         const context = github.context;
-
-        // Get branch name from ref
-        const branchName = context.ref.replace('refs/heads/', '');
-
-        // Find pull requests associated with the branch
-        const { data: pullRequests } = await octokit.rest.pulls.list({
-            ...context.repo,
-            state: 'open',
-            head: `${context.repo.owner}:${branchName}`
-        });
-
-        if (pullRequests.length === 0) {
-            core.setFailed('No related pull request found.');
-            return;
-        }
-
-        // Assuming you want to comment on the first related pull request
-        const pullRequestNumber = pullRequests[0].number;
 
         // Your message
         const message = "\<\!-- MISRA C REPORT --\>\n" +
@@ -30910,18 +30927,14 @@ async function run() {
             "| [lib/src/cbor/cbor\\_reader/cbor\\_reader\\_collections.c](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup#diff-bGliL3NyYy9jYm9yL2Nib3JfcmVhZGVyL2Nib3JfcmVhZGVyX2NvbGxlY3Rpb25zLmM=) | 87.50% | [1 Missing :warning: ](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup) |\n" +
             "| [lib/src/cbor/cbor\\_reader/cbor\\_reader\\_numeric.c](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup#diff-bGliL3NyYy9jYm9yL2Nib3JfcmVhZGVyL2Nib3JfcmVhZGVyX251bWVyaWMuYw==) | 0.00% | [1 Missing :warning: ](https://app.codecov.io/gh/Biglup/cardano-c/pull/23?src=pr&el=tree&utm_medium=referral&utm_source=github&utm_content=comment&utm_campaign=pr+comments&utm_term=Biglup) |"
 
-        await octokit.rest.issues.createComment({
-            ...context.repo,
-            issue_number: pullRequestNumber,
-            body: message
-        });
 
+        await updateMISRAComment(octokit, context, message);
     } catch (error) {
         core.setFailed(error.message);
     }
 }
 
-run().catch();
+run().catch(core.setFailed);
 })();
 
 module.exports = __webpack_exports__;
