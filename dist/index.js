47b431cda54eacd5008fff557649961026fec150
Luis Daniel Bianchi <luisdaniel.bianchicordova@gmail.com>
2024-04-01 02:07:59 +0800

    build

--- dist/index.js
+++ dist/index.js
@@ -74043,31 +74043,27 @@ class TransferManager {
 
 
 
-const BASE_URL = 'https://storage.googleapis.com/misra-c/'; // TODO: Remove hardcoded URL.
+const BASE_URL = 'https://storage.googleapis.com/misra-c/';
 
-// Creates a client
 const storage = new Storage();
 
 async function computeFileHashFromBuffer(buffer) {
-    const crypto = require('crypto');
-    const hash = crypto.createHash('md5');
+    const hash = external_crypto_.createHash('md5');
     hash.update(buffer);
     return hash.digest('hex');
 }
 
 async function readFileIntoBuffer(filename) {
-    return external_fs_.readFile(filename);
+    return external_fs_.promises.readFile(filename);
 }
 
 async function uploadFile(bucketName, filename, destination) {
     const buffer = await readFileIntoBuffer(filename);
     const hash = await computeFileHashFromBuffer(buffer);
-
     console.log(`File hash: ${hash}`);
 
-    const file = storage.bucket(bucketName).file(destination);
     try {
-        await file.save(buffer, {
+        await storage.bucket(bucketName).file(destination).save(buffer, {
             metadata: {
                 contentType: 'application/pdf',
             },
@@ -74075,10 +74071,8 @@ async function uploadFile(bucketName, filename, destination) {
         console.log(`Buffer uploaded to ${bucketName}/${destination}`);
     } catch (error) {
         console.error('Error uploading buffer:', error);
-        throw error; // Rethrow or handle the error appropriately
+        throw error;
     }
-
-    console.log(`${filename} uploaded to ${bucketName}/${destination}`);
 
     return BASE_URL + destination;
 }
--- storage/uploadToStorage.js
+++ storage/uploadToStorage.js
@@ -1,32 +1,28 @@
-import {Storage} from '@google-cloud/storage';
+import { Storage } from '@google-cloud/storage';
 import crypto from 'crypto';
-import fs from 'fs';
+import { promises as fsPromises } from 'fs';
 
-const BASE_URL = 'https://storage.googleapis.com/misra-c/'; // TODO: Remove hardcoded URL.
+const BASE_URL = 'https://storage.googleapis.com/misra-c/';
 
-// Creates a client
 const storage = new Storage();
 
 async function computeFileHashFromBuffer(buffer) {
-    const crypto = require('crypto');
     const hash = crypto.createHash('md5');
     hash.update(buffer);
     return hash.digest('hex');
 }
 
 async function readFileIntoBuffer(filename) {
-    return fs.readFile(filename);
+    return fsPromises.readFile(filename);
 }
 
 export async function uploadFile(bucketName, filename, destination) {
     const buffer = await readFileIntoBuffer(filename);
     const hash = await computeFileHashFromBuffer(buffer);
-
     console.log(`File hash: ${hash}`);
 
-    const file = storage.bucket(bucketName).file(destination);
     try {
-        await file.save(buffer, {
+        await storage.bucket(bucketName).file(destination).save(buffer, {
             metadata: {
                 contentType: 'application/pdf',
             },
@@ -34,10 +30,8 @@ export async function uploadFile(bucketName, filename, destination) {
         console.log(`Buffer uploaded to ${bucketName}/${destination}`);
     } catch (error) {
         console.error('Error uploading buffer:', error);
-        throw error; // Rethrow or handle the error appropriately
+        throw error;
     }
-
-    console.log(`${filename} uploaded to ${bucketName}/${destination}`);
 
     return BASE_URL + destination;
 }
